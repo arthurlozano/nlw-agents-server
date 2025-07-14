394627f34302d534a12e6e5df78a08187fa32d62
arthurlozano <athlonlozano@gmail.com>
2025-07-13 22:22:17 -0300

    feat: add audio-chunks and upload-audio to gemini, add gemini and auto answer

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,24 @@
+import { fastifyCors } from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
 import { fastify } from 'fastify'
 import {
   serializerCompiler,
   validatorCompiler,
-  type ZodTypeProvider
- } from 'fastify-type-provider-zod'
-import { fastifyCors } from '@fastify/cors'
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
 import { env } from './env.ts'
-import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts'
 import { createRoomRoute } from './http/routes/create-room.ts'
 import { getRoomQuestions } from './http/routes/get-room-questions.ts'
-import { createQuestionRoute } from './http/routes/create-question.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { uploadAudioRoute } from './http/routes/upload-audio.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-  origin: '*'
+  origin: '*',
 })
+app.register(fastifyMultipart)
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
@@ -28,6 +31,6 @@ app.register(getRoomsRoute)
 app.register(createRoomRoute)
 app.register(getRoomQuestions)
 app.register(createQuestionRoute)
-
+app.register(uploadAudioRoute)
 
 app.listen({ port: env.PORT })
